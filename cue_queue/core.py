672403199d1be78d19f521fd1305fe0f3506cb6d
JacksonMaxfield <jmaxfieldbrown@gmail.com>
2021-09-27 22:15:01 -0700

    Start with peak finding

--- cue_queue/core.py
+++ cue_queue/core.py
@@ -2,17 +2,21 @@
 # -*- coding: utf-8 -*-
 
 import logging
-from typing import TYPE_CHECKING, Iterable, Optional, Type, Union
+from typing import Iterable, List, Optional, Type, Union
 
 import fsspec
+import numpy as np
 from cdp_backend.pipeline.transcript_model import Transcript
+from scipy.signal import find_peaks
 from sentence_transformers import SentenceTransformer
+from sentence_transformers.util import cos_sim
 from tqdm import tqdm
 
 from .incremental_average import IncrementalAverage
 
-if TYPE_CHECKING:
-    import numpy as np
+###############################################################################
+
+DEFAULT_TRANSFORMER = "sentence-transformers/paraphrase-xlm-r-multilingual-v1"
 
 ###############################################################################
 
@@ -26,11 +30,42 @@ def _load_transformer(
 ) -> SentenceTransformer:
     # Load or pass on transformer
     if transformer is None:
-        return SentenceTransformer(
-            "sentence-transformers/paraphrase-xlm-r-multilingual-v1"
-        )
+        return SentenceTransformer(DEFAULT_TRANSFORMER)
 
     return transformer
+
+
+def _load_transcript(
+    transcript: Union[str, Transcript],
+) -> Transcript:
+    # Read transcript
+    if isinstance(transcript, str):
+        with fsspec.open(transcript, "r") as open_resource:
+            return Transcript.from_json(open_resource.read())
+
+    return transcript
+
+
+def _load_encoding(
+    encoding: Union[str, np.ndarray],
+) -> np.ndarray:
+    # Read
+    if isinstance(encoding, str):
+        return np.load(encoding)
+
+    return encoding
+
+
+def _get_optional_display_iter(
+    iterable: Iterable,
+    display_progress: bool = True,
+    message: Optional[str] = None,
+) -> Iterable:
+    # Get iterator
+    if display_progress:
+        return tqdm(iterable, message)
+
+    return iterable
 
 
 def get_average_delimiter_encoding_for_transcript(
@@ -49,7 +84,7 @@ def get_average_delimiter_encoding_for_transcript(
     transformer: Optional[Type[SentenceTransformer]]
         An optional transformer to use for generating encodings
         from the delimiter sentences.
-        Default: None (use sentence-transformers/paraphrase-xlm-r-multilingual-v1)
+        Default: None (use DEFAULT_TRANSFORMER)
     strict: bool
         When True, will raise on any transcript error.
         Default: False (skip problematic sections)
@@ -69,11 +104,7 @@ def get_average_delimiter_encoding_for_transcript(
     incremental_average = IncrementalAverage()
 
     # Read transcript
-    if isinstance(transcript, str):
-        with fsspec.open(transcript, "r") as open_resource:
-            loaded_transcript = Transcript.from_json(open_resource.read())
-    else:
-        loaded_transcript = transcript
+    loaded_transcript = _load_transcript(transcript)
 
     # Check section annotations are provided
     if (
@@ -125,7 +156,7 @@ def get_average_delimiter_encoding_for_corpus(
     transformer: Optional[Type[SentenceTransformer]]
         An optional transformer to use for generating encodings
         from the delimiter sentences.
-        Default: None (use sentence-transformers/paraphrase-xlm-r-multilingual-v1)
+        Default: None (use DEFAULT_TRANSFORMER)
     strict: bool
         When True, will raise on any transcript error.
         Default: False (skip problematic transcripts)
@@ -145,13 +176,12 @@ def get_average_delimiter_encoding_for_corpus(
     incremental_average = IncrementalAverage()
 
     try:
-        # Get iterator
-        if display_progress:
-            iterator = tqdm(transcripts, "Transcripts processed")
-        else:
-            iterator = transcripts
-
         # Iterate transcripts
+        iterator = _get_optional_display_iter(
+            transcripts,
+            display_progress=display_progress,
+            message="Transcripts processed",
+        )
         for transcript in iterator:
             incremental_average.update(
                 get_average_delimiter_encoding_for_transcript(
@@ -173,8 +203,46 @@ def get_average_delimiter_encoding_for_corpus(
     return incremental_average.average
 
 
-def segment(
+def segment_transcript(
     transcript: Union[str, Transcript],
     delimiter_encoding: Union[str, "np.ndarray"],
+    transformer: Optional[Type[SentenceTransformer]] = None,
+    overwrite_annotations: bool = True,
+    display_progress: bool = True,
 ) -> Transcript:
-    pass
+    # Load or use provided transformer and encoding
+    loaded_transformer = _load_transformer(transformer)
+    loaded_delimiter_encoding = _load_encoding(delimiter_encoding)
+
+    # Read transcript
+    loaded_transcript = _load_transcript(transcript)
+
+    # Check section annotations do not exist (or overwrite False)
+    if (
+        loaded_transcript.annotations is not None
+        and loaded_transcript.annotations.sections is not None
+        and not overwrite_annotations
+    ):
+        raise KeyError(
+            "Transcript has existing section annotations "
+            "and `overwrite_annotations` was not set to True."
+        )
+
+    # Iter all sentences, get encoding, and store cos sim to average delimiter encoding
+    similarities: List[np.ndarray] = []
+    iterator = _get_optional_display_iter(
+        loaded_transcript.sentences,
+        display_progress=display_progress,
+        message="Sentences processed",
+    )
+    for sentence in iterator:
+        # Get this sentence encoding and get distance from each section embedding
+        sentence_encoding = loaded_transformer.encode(sentence.text)
+        similarities.append(
+            cos_sim(loaded_delimiter_encoding, sentence_encoding).numpy()[0, 0]
+        )
+
+    # Attempt to get segmentation breaks using local maximas
+    peaks, _ = find_peaks(np.asarray(similarities))
+
+    return peaks, loaded_transcript
